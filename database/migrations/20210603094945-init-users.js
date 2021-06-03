18ff5df21e8107c81e5692649e16c4418bf817da
taozhi <805446529@qq.com>
2021-06-03 17:59:29 +0800

    m

--- database/migrations/20210603094945-init-users.js
+++ database/migrations/20210603094945-init-users.js
@@ -1,21 +1,27 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
-
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    // 在执行数据库升级时调用的函数，创建 users 表
+    up: async(queryInterface, Sequelize) => {
+        const {
+            INTEGER,
+            DATE,
+            STRING,
+        } = Sequelize;
+        await queryInterface.createTable('users', {
+            id: {
+                type: INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            name: STRING(30),
+            age: INTEGER,
+            created_at: DATE,
+            updated_at: DATE,
+        });
+    },
+    // 在执行数据库降级时调用的函数，删除 users 表
+    down: async queryInterface => {
+        await queryInterface.dropTable('users');
+    },
 };
